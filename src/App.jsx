c311d914886aa4d0e7edae497f3e648f74780c85
Marc Jersey Castro <marcjerseycastro@gmail.com>
2025-01-17 13:19:07 +0800

    backend done niceuu

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import Die from "./components/Die.jsx"
 
 import { useState } from "react"
+import Confetti from "react-confetti"
 
 export default function App()
 {
@@ -19,51 +20,90 @@ export default function App()
 
     return newDice;
   }
-
-  /*  function GenerateRandomDice()
-   {
-     return new Array(10)
-       .fill(0)
-       .map(() => ({
-         value: Math.ceil(Math.random() * 6),
-         isHeld: false
-       }))
-   }
-  */
-
-  const [dice, setDice] = useState(GenerateRandomDice)
+  const [dice, setDice] = useState(() => GenerateRandomDice())
+  const [counter, setCounter] = useState(0);
 
   function RollDice()
   {
-    setDice(GenerateRandomDice)
+    if (!isGameWon)
+    {
+      setCounter(function (prevState) { return prevState + 1 })
+      setDice(function (prevState)
+      {
+        return prevState.map(function (die)
+        {
+          return die.isHeld ? die : { ...die, value: Math.floor(Math.random() * 6) + 1 }
+        })
+      })
+    }
+    else
+    {
+      setDice(GenerateRandomDice);
+      setCounter(0)
+    }
+
+    console.log("COUNTER IS: " + counter)
   }
+
+
+
+
+  function Hold(id)
+  {
+    setDice(oldDice => oldDice.map(die =>
+      die.id === id ?
+        { ...die, isHeld: !die.isHeld } :
+        die
+    ))
+  }
+
+
+
+  const isGameWon = dice.every(die => die.isHeld) &&
+    dice.every(die => die.value === dice[0].value)
+  if (isGameWon)
+  {
+    console.log("Game won!")
+  }
+
 
   return (
     <>
+
+      {isGameWon &&
+
+        <Confetti />
+      }
       <div className="grid grid-cols-5 gap-4 bg-zinc-50 rounded-lg px-[2rem] pt-[2vh] pb-[4vh]">
         <div className="col-span-5 flex justify-center flex-col text-center">
-          <h1 className="text-4xl font-bold mb-8">Tenzies</h1>
+          <h1 className="text-4xl font-bold mb-8">{!isGameWon ? "Tenzies" : "You Won!"}</h1>
           <div className="max-w-lg mx-auto">
-            <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls</p>
+            <p>{isGameWon ? `You did ${counter} rerolls to win the game` : "Roll until all dice are the same. Click each die to freeze it at its current value between rolls"}</p>
           </div>
         </div>
 
         {dice.map(function (dieObj)
         {
-          return <Die key={dieObj.id} value={dieObj.value} />
+          return (
+
+            <Die key={dieObj.id}
+              value={dieObj.value}
+              isHeld={dieObj.isHeld}
+              hold={function () { return Hold(dieObj.id) }}
+            />
+          )
+
         })}
-
-        {/*    {dice.map(num => <Die value={num} />)} */}
-
 
         <div className="col-span-5 flex justify-center mt-9">
           <button className="bg-indigo-500 text-white shadow-md text-[2rem] font-sans rounded-2xl px-9 py-2"
-            onClick={RollDice}>Roll</button>
+            onClick={RollDice}>{isGameWon ? "New Game" : "Roll"}</button>
         </div>
       </div>
 
-
-
+      <footer className="absolute text-white bottom-7">
+        &copy; {new Date().getFullYear()} by MJ.
+      </footer>
     </>
   )
 }
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,8 +1,16 @@
 export default function Die(props)
 {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    };
+
     return (
-        <button className="size-[5rem] bg-slate-50 shadow-md text-[3rem] font-bold font-sans rounded-2xl ">
+        <button
+            className="size-[5rem] bg-slate-50 shadow-md text-[3rem] font-bold font-sans rounded-2xl"
+            style={styles}
+            onClick={props.hold}
+        >
             {props.value}
         </button>
-    )
+    );
 }
